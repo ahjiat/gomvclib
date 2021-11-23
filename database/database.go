7938d03e1a4fac3a5c20bdab550877a711a26822
ahjiat <ah_jiat@hotmail.com>
2021-11-23 10:12:52 +0000

    add comment

--- database/database.go
+++ database/database.go
@@ -6,12 +6,26 @@ import (
 	"gorm.io/gorm/logger"
 )
 
+type DB = gorm.DB
+
 var container = map[string]*gorm.DB{}
 
 func AddDBConnection(sessionName string, dsn string) {
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{
 		Logger: logger.Default.LogMode(logger.Silent),
 	}); if err != nil { panic(err) }
+	/*
+	sqlDB, err := db.DB(); if err != nil { panic(err) }
+	sqlDB.SetMaxIdleConns(10)
+	sqlDB.SetMaxOpenConns(10)
+	go func() {
+		for i:=0;i<1000;i++  {
+			stats := sqlDB.Stats()
+			fmt.Println(stats)
+			time.Sleep(time.Second)
+		}
+	}()
+	*/
 	container[sessionName] = db
 }
 func GetSession(sessionName string) *gorm.DB {
